43da7d4e35b26f418b04cfb5a64b7a0775984006
DESKTOP-8IUR1AM\Glow Computers <aman.mirza9@yahoo.com>
2025-04-05 11:50:11 -0700

    feat: working on mt table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,119 +1,67 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {
-  distinctUntilChanged,
-  distinctUntilKeyChanged,
-  from,
-  fromEvent,
-  of,
-} from 'rxjs';
-import { CommonModule } from '@angular/common';
-import { NewObservableComponent } from './new-observable/new-observable.component';
-import { FunctionObservableComponent } from './function-observable/function-observable.component';
-import { functionObs, functionObservable } from './service/Function';
-import { CancellingObservableComponent } from './cancelling-observable/cancelling-observable.component';
-import { OperatorsBasicsComponent } from './operators-basics/operators-basics.component';
-import { NzButtonModule } from 'ng-zorro-antd/button';
 import { MtTableComponent } from './mt-table/mt-table.component';
+
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    NewObservableComponent,
-    CommonModule,
-    FunctionObservableComponent,
-    CancellingObservableComponent,
-    OperatorsBasicsComponent,
-    NzButtonModule,
-    MtTableComponent,
-  ],
+  imports: [RouterOutlet, MtTableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrl: './app.component.less',
 })
-// export class AppComponent {
-//   title = 'angular-app';
-//   @ViewChild('click-btn', { static: true }) clickBtn!: ElementRef;
-
-//   postArray = [
-//     { title: 'John 1', description: 'abc 1' },
-//     { title: 'John 2', description: 'abc 2' },
-//     { title: 'John 3', description: 'abc 3' },
-//   ];
-
-//   postArrayObservable$ = from(this.postArray);
-//   promise = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       console.log('===', this.postArray);
-
-//       resolve('resolve');
-//     }, 3000);
-//   });
-//   promiseObservable$ = from(this.promise);
-//   constructor() {
-//     console.log('this.promiseObservable$:', this.promiseObservable$);
-//     this.postArrayObservable$.subscribe({
-//       next: (value) => console.log(value),
-//       error: (error) => console.error('Error: ' + error),
-//       complete: () => console.log('Complete'),
-//     });
-//     this.promiseObservable$.subscribe({
-//       next: (value) => console.log(value),
-//       error: (error) => console.error('Error: ' + error),
-//       complete: () => console.log('Complete'),
-//     });
-//   }
-
-//   ngAfterViewInit() {
-//     fromEvent(document.getElementById('click-btn')!, 'click').subscribe({
-//       next: (value) => console.log(value),
-//       error: (error) => console.error('Error: ' + error),
-//       complete: () => console.log('Complete click'),
-//     });
-//   }
-// }
 export class AppComponent {
-  counts: any;
-  // constructor() {
-  //   console.log('Before Function Call');
-  //   console.log(functionObs());
-  //   console.log(functionObs());
-  //   console.log('After Function Call');
-  //   console.log('Before Observable Call');
-  //   functionObservable.subscribe((data) => {
-  //     console.log(data);
-  //   });
-  //   console.log('After Observable Call');
-  // }
-
-  // ngOnInit() {
-  //   debugger;
-  //   const totallyDifferentBuilds$ = of(
-  //     { name: 'Alice', age: 25 },
-  //     { name: 'Alice', age: 25 },
-  //     { name: 'Bob', age: 30 },
-  //     { name: 'Bob', age: 30 },
-  //     { name: 'Charlie', age: 25 },
-  //     { name: 'Charlie1', age: 25 }
-  //   )
-  //     .pipe(distinctUntilKeyChanged('age'))
-  //     .subscribe((x) => console.log(x));
-  // }
-
-  columns = [
-    { name: 'Name', value: 'name' },
-    { name: 'Age', value: 'age' },
-    { name: 'Country', value: 'country' },
+  title = 'angular-app1';
+  columns: any[] = [
+    { name: 'ID', value: 'id', sortKey: 'id', default: true },
+    {
+      name: 'Name',
+      value: 'name',
+      sortKey: 'name',
+      searchKey: 'name',
+      default: true,
+    },
+    { name: 'Age', value: 'age', sortKey: 'age', default: true },
+    { name: 'Address', value: 'address', default: false },
+    { name: 'Email', value: 'email', searchKey: 'email', default: true },
   ];
 
-  data = [
-    { name: 'John', age: 28, country: 'USA' },
-    { name: 'Jane', age: 22, country: 'UK' },
-    { name: 'Mike', age: 32, country: 'Canada' },
+  tableData = [
+    {
+      id: 1,
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      email: 'john@example.com',
+    },
+    {
+      id: 2,
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      email: 'jim@example.com',
+    },
+    {
+      id: 3,
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      email: 'joe@example.com',
+    },
+    {
+      id: 4,
+      name: 'Jim Red',
+      age: 32,
+      address: 'London No. 2 Lake Park',
+      email: 'jim2@example.com',
+    },
   ];
 
-  isLoading = false;
   onCellClick(event: any) {
-    console.log(event);
+    console.log('Cell clicked:', event);
+  }
+
+  onTableEvent(event: any) {
+    console.log('Table event:', event);
+    // Handle pagination, sorting, or filtering changes here
+    // Typically you would make an API call with the new parameters
   }
 }
--- src/app/mt-table/mt-table.component.ts
+++ src/app/mt-table/mt-table.component.ts
@@ -1,57 +1,299 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { NzTableModule, NzTableQueryParams } from 'ng-zorro-antd/table';
-import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { Workbook } from 'exceljs';
+import { saveAs } from 'file-saver';
 import { CommonModule } from '@angular/common';
+
+export interface CustomColumn {
+  name: string;
+  value: string;
+  sortKey?: string;
+  searchKey?: string;
+  alignment?: 'left' | 'right' | 'center';
+  headerAlignment?: 'left' | 'right' | 'center';
+  width?: number;
+  clickEvent?: boolean;
+  default?: boolean;
+  preventClick?: boolean;
+}
+
+export interface MtCustomColumn extends CustomColumn {
+  id?: number;
+}
+
+export interface MtTableSort {
+  [key: string]: 'ascend' | 'descend' | null;
+}
+
+export interface TablePagination {
+  selected: number;
+  size: number;
+  total: number;
+}
+
+export interface MtTableEvent {
+  pagination: TablePagination;
+  sort: MtTableSort;
+  search: Record<string, string>;
+}
+
 @Component({
   selector: 'app-mt-table',
-  standalone: true,
-  imports: [NzTableModule, NzPaginationModule, CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NzTableModule,
+    NzModalModule,
+    NzIconModule,
+    NzButtonModule,
+    NzLayoutModule,
+    NzGridModule,
+    NzSelectModule,
+    NzInputModule,
+    NzCheckboxModule,
+    NzAlertModule,
+    CdkDrag,
+    CdkDropList,
+  ],
   templateUrl: './mt-table.component.html',
-  styleUrl: './mt-table.component.css',
+  styleUrl: './mt-table.component.less',
 })
 export class MtTableComponent {
+  _data: Array<any> = [];
+
   @Input() selectable = true;
   @Input() customizable = true;
-  @Input() columns: Array<any> = [];
-  @Input() data: Array<any> = [];
+  @Input() isReload = true;
+  @Input() isExport = true;
   @Input() isLoading = false;
+  @Input() moduleId?: number;
+  @Input() columns: Array<CustomColumn> = [];
+  @Input() set data(value: Array<any>) {
+    value?.forEach((x) => (x.checked = false));
+    this._data = value;
+  }
   @Input() serverPagination = false;
   @Input() pageSize = 10;
-  @Input() isExport = true;
-  @Input() exportFilename = 'data-export';
+  @Input() exportFileName = 'export';
 
-  @Output() cellClick = new EventEmitter<{ row: any; column: any }>();
+  @Output() cellClick = new EventEmitter<{
+    item: any;
+    column: MtCustomColumn;
+  }>();
+  @Output() tableEvent = new EventEmitter<MtTableEvent>();
 
-  pagination = { selected: 1, size: 10, total: 0 };
-  customColumns: any = [];
-  search: any = {};
+  search: Record<string, string> = {};
+  sort: MtTableSort = {};
+  pagination: TablePagination = { selected: 1, size: 10, total: 0 };
 
-  _data: Array<any> = [];
-  rangeTemplate: any;
+  customColumns: MtCustomColumn[] = [];
+  isColumnConfigModalVisible = false;
+  available: MtCustomColumn[] = [];
+  shown: MtCustomColumn[] = [];
+  searchBoxes: string[] = [];
+
+  dragStarted = false;
+  loading = { saveCol: false, col: false, reset: false, refresh: false };
 
   ngOnInit() {
-    this.customColumns = this.columns;
-    this._data = this.data;
     this.pagination.size = this.pageSize;
+    this.setupColumns();
   }
-  onSearch(event: KeyboardEvent, column: string) {
-    const input = event.target as HTMLInputElement;
-    if (input && input.value) {
-      this.search[column] = input.value;
+
+  setupColumns() {
+    if (this.columns) {
+      this.customColumns = this.columns.map((x) => ({
+        name: x.name,
+        value: x.value,
+        sortKey: x.sortKey,
+        searchKey: x.searchKey,
+        alignment: x.alignment,
+        headerAlignment: x.headerAlignment,
+        width: x.width,
+        clickEvent: x.clickEvent,
+        default: x.default !== false,
+        preventClick: x.preventClick,
+      }));
+    }
+
+    this.resetConfig();
+  }
+
+  resetConfig() {
+    this.shown = this.customColumns.filter((x) => x.default);
+    this.available = this.customColumns.filter((x) => !x.default);
+  }
+
+  onSearch(event: Event | KeyboardEvent, columnKey: string) {
+    if (!(event.target instanceof HTMLInputElement)) return;
+
+    const regex = /^[a-zA-Z0-9\s\-_@.#&+]*$/;
+    if (!regex.test(event.target.value)) {
+      // this.notificationSvc.error('Invalid search parameters');
+      return;
+    }
+
+    if (
+      this.search[columnKey] === event.target.value ||
+      (!this.search[columnKey] && !event.target.value)
+    )
+      return;
+    this.search[columnKey] = event.target.value;
+
+    if (this.pagination.selected === 1) {
+      this.tableEvent.emit(this.makeTableEvent());
+    } else {
+      this.pagination.selected = 1;
     }
   }
-  onRowClick(row: any, column: any) {
-    this.cellClick.emit({ row, column });
+
+  makeTableEvent(): MtTableEvent {
+    return {
+      pagination: this.pagination,
+      sort: this.sort,
+      search: this.search,
+    };
   }
 
-  onQueryParams(event: NzTableQueryParams) {}
+  onQueryParams(event: NzTableQueryParams) {
+    this.tableEvent.emit(this.makeTableEvent());
+  }
 
   onRefresh() {
-    this._data = [...this.data];
-    this.pagination.selected = 1;
+    for (const key of Object.keys(this.sort)) {
+      this.sort[key] = null;
+    }
+
+    for (const key of Object.keys(this.search)) {
+      this.search[key] = '';
+    }
+
+    for (const key in this.searchBoxes) {
+      this.searchBoxes[key] = '';
+    }
+
+    if (this.pagination.selected === 1) {
+      this.tableEvent.emit(this.makeTableEvent());
+    } else {
+      this.pagination.selected = 1;
+    }
   }
 
-  trackByValue(index: number, item: any) {
-    return item.value;
+  onCellClick(item: any, column: MtCustomColumn) {
+    this.cellClick.emit({ item, column });
+  }
+
+  onCheckChange(value: boolean, row: any) {
+    this._data.forEach((x) => {
+      if (x !== row) x.checked = false;
+    });
+  }
+
+  onApplyColumnConfig() {
+    this.customColumns = [...this.shown, ...this.available];
+    this.isColumnConfigModalVisible = false;
+  }
+
+  onExcelExport() {
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('Sheet 1');
+    const headers = this.customColumns
+      .filter((x) => x.default)
+      .map((col) => col.name);
+    const dataKeys = this.customColumns
+      .filter((x) => x.default)
+      .map((col) => col.value);
+    worksheet.addRow(headers);
+
+    this._data?.forEach((item) => {
+      const row: any[] = [];
+      dataKeys?.forEach((key) => {
+        row.push(item[key]);
+      });
+      worksheet.addRow(row);
+    });
+
+    this.customColumns
+      .filter((x) => x.default)
+      .forEach((col, index) => {
+        let maxColumnLength = col.name.length;
+        this._data?.forEach((item) => {
+          const cellData = item[col.value]?.toString() || '';
+          if (cellData.length > maxColumnLength) {
+            maxColumnLength = cellData.length;
+          }
+        });
+        worksheet.columns[index].width = maxColumnLength + 2;
+      });
+
+    workbook.xlsx.writeBuffer().then((buffer) => {
+      const blob = new Blob([buffer], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      });
+      saveAs(
+        blob,
+        `${this.exportFileName}-${new Date().toLocaleDateString()}.xlsx`
+      );
+    });
+  }
+
+  onCancel() {
+    this.isColumnConfigModalVisible = false;
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+
+    this.shown = this.shown.map((x) => {
+      x.default = true;
+      return x;
+    });
+
+    this.available = this.available.map((x) => {
+      x.default = false;
+      return x;
+    });
+  }
+
+  addCustom(value: MtCustomColumn, index: any) {
+    value.default = true;
+    this.shown = [...this.shown, value];
+    this.available.splice(index, 1);
+  }
+
+  deleteCustom(value: MtCustomColumn, index: any) {
+    value.default = false;
+    this.available = [...this.available, value];
+    this.shown.splice(index, 1);
   }
 }
